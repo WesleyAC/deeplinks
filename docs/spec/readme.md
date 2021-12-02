da0014311aa09b98d5bbb89a13c8a0554d4cd9c7
Wesley Aptekar-Cassels <me@wesleyac.com>
2021-12-02 14:12:47 +0800

    [docs] Add specs for fragment URLs.

---
+++ docs/spec/readme.md
@@ -0,0 +1,3 @@
+# Fragment URL Spec
+
+These docs describe the format of the fragment URLs used by `deeplinks.js`. The first character of the fragment is a version number. For historical reasons, both [version zero](/docs/spec/v0.md) and [version one](/docs/spec/v1.md) begin with the character `1`.
--- src/versions/1.ts
+++ src/versions/1.ts
@@ -1,48 +1,8 @@
 import { cyrb53 } from '../util/cyrb53';
 import { fromNumber } from '../util/base64';
 
-// Version 1 Fragment Format (UNSTABLE)
-//
-// fragment  → "1", [ "." ], selection, { ",",  selection } ;
-// selection → sel-long | sel-short;
-// sel-long  → hash, ":", offset, ".", hash, ":", offset, [ dedupe ] ;
-// sel-short → hash, ":", offset, ":", offset, [ "~", dedupe ] ;
-// dedupe    → "~", { "s" | "e" }-, "~", offset, "~", offset ;
-// offset    → { "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" }- ;
-// hash      → { "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" |
-//               "A" | "B" | "C" | "D" | "E" | "F" | "G" | "H" | "I" | "J" |
-//               "K" | "L" | "M" | "N" | "O" | "P" | "Q" | "R" | "S" | "T" |
-//               "U" | "V" | "W" | "X" | "Y" | "Z" | "a" | "b" | "c" | "d" |
-//               "e" | "f" | "g" | "h" | "i" | "j" | "k" | "l" | "m" | "n" |
-//               "o" | "p" | "q" | "r" | "s" | "t" | "u" | "v" | "w" | "x" |
-//               "y" | "z" | "-" | "_" }- ;
-//
-// A fragment describes a list of selections. A selection is stored as a hash
-// of the text contents of a selected node, as well as a character offset into
-// the text.
-//
-// The short selection format is used when both hashes refer to the same node.
-//
-// The hash formula is cyrb53, which is just a thing some rando on stack
-// overflow made, base64-encoded (using a non-standard alphabet). In practice,
-// a hash can be at most nine characters, since cyrb53 is a 53-bit hash (due to
-// silly javascript fun).
-//
-// The deduplication section of the fragment is used in the case where there
-// are multiple elements on the page with the same contents as the start or end
-// node. It describes the full list of duplicate elements, and which ones are
-// the correct start and end nodes. A "s" or "e" indicates a node with the same
-// hash as the start or end node (respectively). The fist offset is the offset
-// of the start node, and the second offset is the end node. In the case where
-// the start and end nodes are the same, "s" must be used to describe the node
-// (it's possible that in the future, a more condensed version of this will be
-// used that simply uses three integers).
-//
-// The hash that comes first in the url must also be first in the document.
-//
-// The main problem this has is that changing a single character in a long
-// paragraph breaks all links to that paragraph, but it works pretty well for a
-// lot of cases.
+// See docs/spec/v1.md for what this code implements.
+// It must also be compatible with docs/spec/v0.md.
 
 // See https://dom.spec.whatwg.org/#interface-node
 // The minifier isn't smart enough to know this, so do it ourselves and save
