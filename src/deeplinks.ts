8f3184d4892398d8b2cf817dc18b97a591f116b6
Wesley Aptekar-Cassels <me@wesleyac.com>
2021-12-07 19:00:58 +0800

    [nfc] Remove DOMContentLoaded listener.
    ES6 modules execute after the DOM is loaded by default, so this listener
    wasn't really doing anything for ue, and was just taking up extra bytes.
    
    See https://developer.mozilla.org/en-US/docs/Web/HTML/Element/script#attr-defer.

--- src/deeplinks.ts
+++ src/deeplinks.ts
@@ -12,28 +12,26 @@ function selectRanges(ranges: Range[]) {
   }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  const fragment = location.hash.slice(1);
+const fragment = location.hash.slice(1);
 
-  if (fragment && !document.getElementById(fragment)) {
-    if (fragment[0] == '1') {
-      selectRanges(v1.fragmentToRangeList(fragment));
-    }
+if (fragment && !document.getElementById(fragment)) {
+  if (fragment[0] == '1') {
+    selectRanges(v1.fragmentToRangeList(fragment));
   }
+}
 
-  // This is in a setTimeout to ensure that the code above does all of its
-  // selection-changing before this executes. This ensures that we don't
-  // clobber changes that we just made (for instance, in the case of a user
-  // on Chrome attempting to open a multiselect url).
-  //
-  // This also allows us to make more careful decisions about rewriting urls
-  // in general — we can explicitly decide when and how to do version bumps,
-  // for instance.
-  setTimeout(() => {
-    document.addEventListener('selectionchange', () => {
-      const fragment = v1.selectionToFragment(document.getSelection() as Selection);
-      // replaceState is used instead of setting location.hash to avoid scrolling.
-      history.replaceState(null, '', location.pathname + fragment);
-    });
-  }, 0);
-});
+// This is in a setTimeout to ensure that the code above does all of its
+// selection-changing before this executes. This ensures that we don't
+// clobber changes that we just made (for instance, in the case of a user
+// on Chrome attempting to open a multiselect url).
+//
+// This also allows us to make more careful decisions about rewriting urls
+// in general — we can explicitly decide when and how to do version bumps,
+// for instance.
+setTimeout(() => {
+  document.addEventListener('selectionchange', () => {
+    const fragment = v1.selectionToFragment(document.getSelection() as Selection);
+    // replaceState is used instead of setting location.hash to avoid scrolling.
+    history.replaceState(null, '', location.pathname + fragment);
+  });
+}, 0);
