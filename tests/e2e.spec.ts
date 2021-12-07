c7e96e73a484d3f2a6a6c0b86c5799440fdf6fc7
Wesley Aptekar-Cassels <me@wesleyac.com>
2021-12-08 04:53:18 +0800

    [tests] Add tests for v2 links.

--- tests/e2e.spec.ts
+++ tests/e2e.spec.ts
@@ -17,6 +17,22 @@ async function testFragment(page: Page, fragment: string, testFn: (Page) => void
 
 test('misc', async ({ page }) => {
   const tests = {
+    '#2JmqE9nH3Z:121:158': ['valueless until you get the screw out', 'short format'],
+    '#2JmqE9nH3Z:121.JmqE9nH3Z:158': ['valueless until you get the screw out', 'long format (but single node)'],
+    '#2J9W3o85TQ:12.3EdKovNLr:11': ['統一碼 💚💙💜🧡💛💚💙💜🧡💛💚💙💜🧡\n\n🐢🐢', 'selecting multiple different nodes, also unicode'],
+    '#26SHlbtTkC:4.CBcmrfV8L:4': ['links.js e2e', 'selecting parent/child nodes'],
+    '#2EdoNr3xj_:0.BLkIVltu0:14': ['uh oh\nidentical text', 'multiple identical nodes, but no disambiguation - start node is unique'],
+    '#27whfBu1TH:0.TxIWFV5Nq:4': ['identical text nodes?\nhmmm', 'multiple identical nodes, but no disambiguation - end node is unique'],
+    '#2EdoNr3xj_:0.7whfBu1TH:21~seeeee~0~2': ['uh oh\nidentical text nodes\nidentical text nodes?\nidentical text nodes\nidentical text nodes?', 'multiple identical nodes, with disambiguation - start node is unique'],
+    '#2BLkIVltu0:0.TxIWFV5Nq:4~ssssse~3~5': ['identical text nodes\nidentical text nodes?\nidentical text nodes\nidentical text nodes?\nhmmm', 'multiple identical nodes, with disambiguation - end node is unique'],
+    '#2BLkIVltu0:0.7whfBu1TH:21~sesesesese~2~7': ['identical text nodes\nidentical text nodes?\nidentical text nodes\nidentical text nodes?\nidentical text nodes\nidentical text nodes?', 'multiple identical nodes, with disambiguation, both start and end nodes are ambiguous'],
+    '#2BLkIVltu0:0:20~sssss~0~1': ['identical text nodes\nidentical text nodes?\nidentical text nodes', 'short fragment with ambiguous nodes, start and end node are different'],
+    '#2BLkIVltu0:0:20~sssss~1': ['identical text nodes', 'short fragment with ambiguous nodes, start and end node are the same'],
+    '#2W00000001:0.W00000001:5': ['', 'nonexistent node'],
+    '#2W00000001:0.W00000002:5': ['', 'nonexistent nodes'],
+    '#2W00000001:0:5': ['', 'nonexistent node (short version)'],
+    '#2W00000001:0.W00000002:5~sse~1~2': ['', 'nonexistent nodes, with disambiguation'],
+    // version 1
     '#1JmqE9nH3Z:121:158': ['valueless until you get the screw out', 'short format, no dot'],
     '#1.JmqE9nH3Z:121:158': ['valueless until you get the screw out', 'short format, with dot'],
     '#1.JmqE9nH3Z:121.JmqE9nH3Z:158': ['valueless until you get the screw out', 'long format (but single node), with dot'],
@@ -52,6 +68,7 @@ test('misc', async ({ page }) => {
 test('multiselect', async ({ page }, testInfo) => {
   const multiselectKnownGoodBrowsers = ['firefox'];
   const tests = {
+    '#293nojgL33:21.3fpc_LoVz:0,3fpc_LoVz:5.VVGZj9Vjq:0': ['bold', 'italic'],
     '#193nojgL33:21.3fpc_LoVz:0,3fpc_LoVz:5.VVGZj9Vjq:0': ['bold', 'italic'],
     '#1.J9W3o85TQ:7:15,J9W3o85TQ:16:18,J9W3o85TQ:26.J9W3o85TQ:28,J9W3o85TQ:36:38,3EdKovNLr:0:18,VIMmaQoVW:229.JmqE9nH3Z:10': ['我是少量的統一碼', '💚', '💚', '💚', ' 🐢🐢🐢\n\n', 'unimportant.\n\nRight'],
   };
